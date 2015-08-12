229ac0e077ac2bd1a1b35ce0358990622a46824c
vurdalakov <vurdalakov@gmail.com>
2015-08-12 09:53:41 +0300

    atoms: -a,-f,-d,-q commands

--- src/Atoms/Application.cs
+++ src/Atoms/Application.cs
@@ -10,12 +10,64 @@
             {
                 for (var i = 0xC000; i <= 0xFFFF; i++)
                 {
-                    var name = GlobalAtomTable.GetRegisteredFormatName((UInt16)i);
-                    if (name != null)
+                    //var name = GlobalAtomTable.GetRegisteredFormatName((UInt16)i);
+                    //if (name != null)
+                    //{
+                    //    Console.WriteLine("{0}\t0x{0:X4}\t{1}", i, name);
+                    //}
+                    try
                     {
-                        Console.WriteLine("{0}\t0x{0:X4}\t{1}", i, name);
+                        var info = GlobalAtomTable.QueryBasicInformation((UInt16)i);
+                        Console.WriteLine("{0}\t0x{0:X4}\t{1}", i, info.Name);
                     }
+                    catch { }
                 }
+            }
+            else if (_commandLineParser.IsOptionSet("a"))
+            {
+                var name = _commandLineParser.GetOptionString("a", null);
+                if (null == name)
+                {
+                    Help();
+                }
+
+                var atom = GlobalAtomTable.Add(name);
+                Console.WriteLine("{0}={1}", name, atom);
+            }
+            else if (_commandLineParser.IsOptionSet("f"))
+            {
+                var name = _commandLineParser.GetOptionString("f", null);
+                if (null == name)
+                {
+                    Help();
+                }
+
+                var atom = GlobalAtomTable.Add(name);
+                Console.WriteLine("{0}={1}", name, atom);
+            }
+            else if (_commandLineParser.IsOptionSet("d"))
+            {
+                var atom = _commandLineParser.GetOptionInt("d", -1);
+                if ((atom < 0) || (atom > UInt16.MaxValue))
+                {
+                    Help();
+                }
+
+                GlobalAtomTable.Delete((UInt16)atom);
+            }
+            else if (_commandLineParser.IsOptionSet("q"))
+            {
+                var atom = _commandLineParser.GetOptionInt("q", -1);
+                if ((atom < 0) || (atom > UInt16.MaxValue))
+                {
+                    Help();
+                }
+
+                var atomBasicInformation = GlobalAtomTable.QueryBasicInformation((UInt16)atom);
+                Console.WriteLine("ReferenceCount={0}", atomBasicInformation.ReferenceCount);
+                Console.WriteLine("Pinned={0}", atomBasicInformation.Pinned);
+                Console.WriteLine("NameLength={0}", atomBasicInformation.NameLength);
+                Console.WriteLine("Name={0}", atomBasicInformation.Name);
             }
             else
             {
@@ -30,7 +82,7 @@
             Console.WriteLine("Atoms {0} | https://github.com/vurdalakov/dostools\n", ApplicationVersion);
             Console.WriteLine("Works with Windows global atom table.\n");
             Console.WriteLine("Usage:\n\tatoms <-command> [-silent]\n");
-            Console.WriteLine("Commands:\n\t-e - enumerates all strings\n");
+            Console.WriteLine("Commands:\n\t-e - enumerates all strings\n\t-a:string - adds string\n\t-f:string - finds string\n\t-d:atom - deletes atom\n\t-q:atom - queries basic information about atom\n");
             Console.WriteLine("Options:\n\t-silent - no error messsages are shown; check exit code\n");
             Console.WriteLine("Exit codes:\n\t0 - command succeeded\n\t1 - command failed\n\t-1 - invalid command line syntax\n");
 
--- src/Atoms/GlobalAtomTable.cs
+++ src/Atoms/GlobalAtomTable.cs
@@ -1,6 +1,7 @@
 ﻿namespace Vurdalakov
 {
     using System;
+    using System.ComponentModel;
     using System.Runtime.InteropServices;
     using System.Text;
 
@@ -12,7 +13,99 @@
             return GetClipboardFormatName(format, stringBuilder, stringBuilder.Capacity) > 0 ? stringBuilder.ToString() : null;
         }
 
+        static public UInt16 Add(String name)
+        {
+            UInt16 atom = 0;
+            ThrowIfFailed(NtAddAtom(name, (UInt32)name.Length, ref atom), "NtAddAtom");
+            return atom;
+        }
+
+        static public UInt16 Find(String name)
+        {
+            UInt16 atom = 0;
+            ThrowIfFailed(NtFindAtom(name, (UInt32)name.Length, ref atom), "NtFindAtom");
+            return atom;
+        }
+
+        static public void Delete(UInt16 atom)
+        {
+            ThrowIfFailed(NtDeleteAtom(atom), "NtDeleteAtom");
+        }
+
+        static public ATOM_BASIC_INFORMATION QueryBasicInformation(UInt16 atom)
+        {
+            var atomBasicInformation = new ATOM_BASIC_INFORMATION();
+
+            var size = Marshal.SizeOf(atomBasicInformation);
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(atomBasicInformation, ptr, false);
+
+            UInt32 returnLength = 0;
+            ThrowIfFailed(NtQueryInformationAtom(atom, 0, ptr, (UInt32)size, ref returnLength), "NtQueryInformationAtom");
+
+            atomBasicInformation = (ATOM_BASIC_INFORMATION)Marshal.PtrToStructure(ptr, typeof(ATOM_BASIC_INFORMATION));
+
+            var bytes = new Byte[1024];
+            Marshal.Copy(ptr, bytes, 0, size);
+
+            Marshal.FreeHGlobal(ptr);
+
+            return atomBasicInformation;
+        }
+
+        private static void ThrowIfFailed(UInt32 ntstatus, String functionName)
+        {
+            if (ntstatus != 0)
+            {
+                var message = String.Format("Function {0}() failed with error {1} (0x{1:X8}).", functionName, ntstatus);
+
+                throw new Win32Exception((Int32)ntstatus, message);
+            }
+        }
+
+        //private Int32 NtStatus2Win32Error(UInt32 ntstatus)
+        //{
+        //        DWORD oldError;
+        //        DWORD result;
+        //        DWORD br;
+        //        OVERLAPPED o;
+
+        //        o.Internal = ntstatus;
+        //        o.InternalHigh = 0;
+        //        o.Offset = 0;
+        //        o.OffsetHigh = 0;
+        //        o.hEvent = 0;
+        //        oldError = GetLastError();
+        //        GetOverlappedResult(NULL, &o, &br, FALSE);
+        //        result = GetLastError();
+        //        SetLastError(oldError);
+        //        return result;
+        //}
+
         [DllImport("user32.dll", SetLastError = true)]
         private static extern Int32 GetClipboardFormatName(UInt32 format, StringBuilder lpszFormatName, Int32 cchMaxCount);
+
+        [DllImport("ntdll.dll", CharSet = CharSet.Unicode)]
+        private static extern UInt32 NtAddAtom(String name, UInt32 length, ref UInt16 atom);
+
+        [DllImport("ntdll.dll", CharSet = CharSet.Unicode)]
+        private static extern UInt32 NtFindAtom(String name, UInt32 length, ref UInt16 atom);
+
+        [DllImport("ntdll.dll")]
+        private static extern UInt32 NtDeleteAtom(UInt16 atom);
+
+        [DllImport("ntdll.dll")]
+        private static extern UInt32 NtQueryInformationAtom(UInt16 atom, Int32 atomInformationClass, IntPtr atomInformation, UInt32 atomInformationLength, ref UInt32 returnLength);
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1, CharSet = CharSet.Auto)]
+        public struct ATOM_BASIC_INFORMATION
+        {
+            public UInt16 ReferenceCount;
+            public UInt16 Pinned;
+            public UInt16 NameLength;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public String Name;
+        }
     }
 }

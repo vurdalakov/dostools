eb0a03f015c91a7c99e707f6857f913995947fb4
vurdalakov <vurdalakov@gmail.com>
2015-08-12 08:31:09 +0300

    atoms: first commit

---
+++ src/Atoms/Application.cs
@@ -0,0 +1,40 @@
+﻿namespace Vurdalakov
+{
+    using System;
+
+    public class Application : DosToolsApplication
+    {
+        protected override Int32 Execute()
+        {
+            if (_commandLineParser.IsOptionSet("e", "enumerate"))
+            {
+                for (var i = 0xC000; i <= 0xFFFF; i++)
+                {
+                    var name = GlobalAtomTable.GetRegisteredFormatName((UInt16)i);
+                    if (name != null)
+                    {
+                        Console.WriteLine("{0}\t0x{0:X4}\t{1}", i, name);
+                    }
+                }
+            }
+            else
+            {
+                Help();
+            }
+
+            return 0;
+        }
+
+        protected override void Help()
+        {
+            Console.WriteLine("Atoms {0} | https://github.com/vurdalakov/dostools\n", ApplicationVersion);
+            Console.WriteLine("Works with Windows global atom table.\n");
+            Console.WriteLine("Usage:\n\tatoms <-command> [-silent]\n");
+            Console.WriteLine("Commands:\n\t-e - enumerates all strings\n");
+            Console.WriteLine("Options:\n\t-silent - no error messsages are shown; check exit code\n");
+            Console.WriteLine("Exit codes:\n\t0 - command succeeded\n\t1 - command failed\n\t-1 - invalid command line syntax\n");
+
+            base.Help();
+        }
+    }
+}
---
+++ src/Atoms/GlobalAtomTable.cs
@@ -0,0 +1,18 @@
+﻿namespace Vurdalakov
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Text;
+
+    public static class GlobalAtomTable
+    {
+        static public String GetRegisteredFormatName(UInt16 format)
+        {
+            var stringBuilder = new StringBuilder(256);
+            return GetClipboardFormatName(format, stringBuilder, stringBuilder.Capacity) > 0 ? stringBuilder.ToString() : null;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern Int32 GetClipboardFormatName(UInt32 format, StringBuilder lpszFormatName, Int32 cchMaxCount);
+    }
+}

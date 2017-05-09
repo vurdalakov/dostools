63f134123676e52977f321ff1d207808f9338472
vurdalakov <vurdalakov@gmail.com>
2017-05-09 14:11:59 +0300

    bigint project added

---
+++ src/BigInt/BigInt.cs
@@ -0,0 +1,28 @@
+﻿namespace Vurdalakov
+{
+    using System;
+    using System.Globalization;
+    using System.Numerics;
+
+    public static class BigInt
+    {
+        public static String FromHex(String hexString)
+        {
+            hexString = hexString.ToLower();
+
+            if (hexString.StartsWith("0x") || hexString.StartsWith("&h"))
+            {
+                hexString = hexString.Substring(2);
+            }
+
+            var bigint = BigInteger.Parse(hexString, NumberStyles.HexNumber);
+            return bigint.ToString("D");
+        }
+
+        public static String ToHex(String decString)
+        {
+            var bigint = BigInteger.Parse(decString, NumberStyles.Integer);
+            return bigint.ToString("X");
+        }
+    }
+}
---
+++ src/BigInt/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Vurdalakov
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Help();
+            }
+
+            switch (args[0])
+            {
+                case "fromhex":
+                    Console.WriteLine("{0}", BigInt.FromHex(args[1]));
+                    break;
+                case "tohex":
+                    Console.WriteLine("{0}", BigInt.ToHex(args[1]));
+                    break;
+                default:
+                    Help();
+                    break;
+            }
+        }
+
+        static void Help()
+        {
+            Console.WriteLine("bigint fromhex <hex string>");
+            Console.WriteLine("bigint tohex <int string>");
+            Environment.Exit(-1);
+        }
+    }
+}
